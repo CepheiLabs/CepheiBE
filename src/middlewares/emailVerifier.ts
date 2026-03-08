8923490c87e4337bfa9714761484678733c288b5
nosaemma21 <emmanuelnosakhare052@gmail.com>
2026-03-08 08:39:14 +0100

    Modified: Finished up the verifyEmail middleware

--- src/middlewares/emailVerifier.ts
+++ src/middlewares/emailVerifier.ts
@@ -1,21 +1,24 @@
 import type { Request, Response, NextFunction } from "express";
-import { UnauthorizedError } from "../errors";
+import { ForbiddenError, UnauthorizedError } from "../errors";
 import * as playerService from "../services/playerService";
 
-const isVerifiedOrWallet = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+const verifyEmail = async (req: Request, res: Response, next: NextFunction) => {
   const player = req.user;
   if (!player || !player.id)
     throw new UnauthorizedError(
       "You are not allowed access to this resource, please login",
     );
 
-  const playerData = playerService.findById(player.id);
+  const playerData = await playerService.findById(player.id);
 
   if (!playerData) throw new UnauthorizedError("User does not exist");
+
+  if (!playerData.isVerifiedEmail)
+    throw new ForbiddenError(
+      "Access denied! Please verify your email to continue",
+    );
+
+  // Everything well?, on to the next
 };
 
-export { isVerifiedOrWallet };
+export { verifyEmail };
